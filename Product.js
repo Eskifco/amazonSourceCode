2cf53ef61d7b322cb59cc32f67a54ae74e49f033
®Eskif <eskifco95@gmail.com>
2022-01-24 14:39:14 +0100

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import styles from "./Product.css";
+
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: state ? 1 : 0,
+    config: { duration: 1000 },
+  });
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+      <div className={styles.container} onClick={() => toggle(!state)}>
+        <animated.div
+          className={styles.text}
+          style={{
+            opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+            scale: x.to({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            }),
+          }}
+        >
+          <button className="button" onClick={addToBasket}>
+            Add to Basket
+          </button>
+        </animated.div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
